2292f7335e6098e73e0167c1b15de5e6efec0966
rohan98k <rohankamath1998@yahoo.com>
2021-05-11 17:49:34 +0530

    Router Rendering: Fixed

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router';
+import { Route, Redirect } from 'react-router-dom';
 
 export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
   return (
     <Route
       {...rest}
-      resnder={() => {
+      render={() => {
         if (!user) {
           return children;
         }
